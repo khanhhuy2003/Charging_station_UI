2fe90f498808d94c02a4d2805b460610a917fe14
khanhhuy2003 <huy90907@gmail.com>
2026-02-23 02:56:11 -0500

    Deleted taskbar

--- view/ui_gen/main_ui.py
+++ view/ui_gen/main_ui.py
@@ -14,7 +14,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(1024, 610)
+        MainWindow.resize(1024, 600)
         font = QtGui.QFont()
         font.setPointSize(10)
         MainWindow.setFont(font)
